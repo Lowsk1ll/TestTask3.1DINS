d52fc18ca58225b53c782c9b475005fad7d4d92a
enjoy1999@yandex.ru <Venya_Tsegelnick_1999>
2020-12-28 00:08:10 +0300

    Добавил закрытие

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,4 +1,5 @@
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,4 +21,6 @@ public class BasicTest {
         company.ChangeLaunguage(driverWrapper.driver);
         Assert.assertEquals(company.checkYear(),"Since 1998");
     }
+    @AfterMethod
+    void end(){driverWrapper.close();}
 }
